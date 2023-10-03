66575c72502497109ba0c63f735fd85f46cc6201
Tasos Stefanou <tasos.stefanou@scigentech.com>
2023-10-03 16:47:41 +0300

    fix(subscription-management-with-stripe): make source email env variable

--- subscription-management-with-stripe/server/utils/emails.js
+++ subscription-management-with-stripe/server/utils/emails.js
@@ -1,6 +1,8 @@
 import AWS from 'aws-sdk';
 AWS.config.update({ region: 'eu-west-1' });
 
+const SOURCE_EMAIL = process.env.SOURCE_EMAIL;
+
 const SESConfig = {
   accessKeyId: process.env.AWS_SES_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SES_SECRET_ACCESS_KEY_ID,
@@ -16,7 +18,7 @@ const sendActivationEmail = (recipientEmail) => {
       ToAddresses: [recipientEmail],
     },
     ReplyToAddresses: [],
-    Source: 'dev@scigentech.com',
+    Source: SOURCE_EMAIL,
     Message: {
       Body: {
         Html: {
@@ -42,75 +44,5 @@ const sendActivationEmail = (recipientEmail) => {
     else console.log('Cnfirmation email sent to:', recipientEmail); // successful response
   });
 };
-
-// const sendResetPasswordEmail = (recipientEmail, resetPasswordToken) => {
-//   console.log(recipientEmail, resetPasswordToken);
-//   const params = {
-//     Destination: {
-//       //   BccAddresses: [],
-//       //   CcAddresses: ['recipient3@example.com'],
-//       ToAddresses: [recipientEmail],
-//     },
-//     ReplyToAddresses: [],
-//     Source: 'dev@scigentech.com',
-//     Message: {
-//       Body: {
-//         Html: {
-//           Charset: 'UTF-8',
-//           Data: `Click  to <a class="ulink" href="${process.env.WEB_APP_URL_FRONT}/reset-password/${resetPasswordToken}" target="_blank">reset your password</a>.`,
-//         },
-//         Text: {
-//           Charset: 'UTF-8',
-//           Data: `Click to reset your password ${process.env.WEB_APP_URL_FRONT}/reset-password/${resetPasswordToken}.`,
-//         },
-//       },
-//       Subject: {
-//         Charset: 'UTF-8',
-//         Data: 'Scigen Technologies - Reset your ePostersLive account password.',
-//       },
-//     },
-//   };
-//   ses.sendEmail(params, function (err, data) {
-//     if (err) {
-//       console.log('Something went wrong with the email sending...');
-//       console.log(err, err.stack);
-//     } // an error occurred
-//     else console.log('Reset password email sent to:', recipientEmail); // successful response
-//   });
-// };
-// const sendAlarmEmail = (message) => {
-//   const params = {
-//     Destination: {
-//       //   BccAddresses: [],
-//       //   CcAddresses: ['recipient3@example.com'],
-//       ToAddresses: [process.env.ALARM_RECEIVER_EMAIL],
-//     },
-//     ReplyToAddresses: [],
-//     Source: 'epl2portal@scigentech.com',
-//     Message: {
-//       Body: {
-//         Html: {
-//           Charset: 'UTF-8',
-//           Data: `${message}`,
-//         },
-//         Text: {
-//           Charset: 'UTF-8',
-//           Data: `${message}`,
-//         },
-//       },
-//       Subject: {
-//         Charset: 'UTF-8',
-//         Data: 'Alarm - ePostersLive v2 Portal!!!',
-//       },
-//     },
-//   };
-//   ses.sendEmail(params, function (err, data) {
-//     if (err) {
-//       console.log('Something went wrong with the alarm email sending...');
-//       console.log(err, err.stack);
-//     } // an error occurred
-//     else console.log('Email was sent to admin'); // successful response
-//   });
-// };
 
 export { sendActivationEmail };
